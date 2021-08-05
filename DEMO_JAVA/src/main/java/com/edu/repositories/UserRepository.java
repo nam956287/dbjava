b4f98524a23d569acd691c72c26f7eade444691d
TechCare <TechCare@DESKTOP-MQBT788>
2021-08-05 11:08:54 +0700

    First commit

--- DEMO_JAVA/src/main/java/com/edu/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edu.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.edu.model.User;
-
-public interface UserRepository extends CrudRepository<User, String> {
-	
-}
